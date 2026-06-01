5f0e8e784fa90c0facbc69373f6513b845117be3
sergey dyakonov <beefeater83@gmail.com>
2026-06-01 23:44:09 +0300

    logged failed mailing

--- backend/src/EventListener/UserLoggedInEventListener.php
+++ backend/src/EventListener/UserLoggedInEventListener.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use App\Event\UserLoggedInEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -12,22 +14,31 @@ use Symfony\Component\Mime\Email;
 #[AsEventListener]
 class UserLoggedInEventListener
 {
-    public function __construct(private MailerInterface $mailer)
-    {
+    public function __construct(
+        private MailerInterface $mailer,
+        #[Autowire(service: 'monolog.logger.security')]
+        private LoggerInterface $logger,
+    ) {
     }
 
     public function __invoke(UserLoggedInEvent $event): void
     {
-        $user = $event->user;
-        $email = (new Email())
-            ->from('no-reply@diakonov-it.com.ua')
-            ->to($user->getEmail())
-            ->subject('Login notification')
-            ->html("
-                <p style='font-size:14px; color:#333;'>
-                Hello <strong>{$user->getName()}</strong>,
-                you have successfully logged-in on security.diakonov-it.com.ua</p>
-        ");
-        $this->mailer->send($email);
+        try {
+            $user = $event->user;
+            $email = (new Email())
+                ->from('no-reply@diakonov-it.com.ua')
+                ->to($user->getEmail())
+                ->subject('Login notification')
+                ->html("
+                    <p style='font-size:14px; color:#333;'>
+                    Hello <strong>{$user->getName()}</strong>,
+                    you have successfully logged-in on security.diakonov-it.com.ua</p>
+            ");
+            $this->mailer->send($email);
+        } catch (\Throwable $exception) {
+            $this->logger->error('Mailing failed', [
+                'reason' => $exception->getMessage(),
+            ]);
+        }
     }
 }
