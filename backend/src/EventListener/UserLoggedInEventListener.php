00a00c5720a580234965a2c20687d92e1179fb73
sergey dyakonov <beefeater83@gmail.com>
2026-06-09 15:13:12 +0300

    async emailes

--- backend/src/EventListener/UserLoggedInEventListener.php
+++ backend/src/EventListener/UserLoggedInEventListener.php
@@ -5,40 +5,26 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use App\Event\UserLoggedInEvent;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use App\Messenger\Message\SendLoginEmailMessage;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsEventListener]
 class UserLoggedInEventListener
 {
     public function __construct(
-        private MailerInterface $mailer,
-        #[Autowire(service: 'monolog.logger.security')]
-        private LoggerInterface $logger,
+        private MessageBusInterface $bus
     ) {
+
     }
 
     public function __invoke(UserLoggedInEvent $event): void
     {
-        try {
-            $user = $event->user;
-            $email = (new Email())
-                ->from('no-reply@diakonov-it.com.ua')
-                ->to($user->getEmail())
-                ->subject('Login notification')
-                ->html("
-                    <p style='font-size:14px; color:#333;'>
-                    Hello <strong>{$user->getName()}</strong>,
-                    you have successfully logged-in on security.diakonov-it.com.ua</p>
-            ");
-            $this->mailer->send($email);
-        } catch (\Throwable $exception) {
-            $this->logger->error('Mailing failed', [
-                'reason' => $exception->getMessage(),
-            ]);
-        }
+        $this->bus->dispatch(
+            new SendLoginEmailMessage(
+                $event->user->getEmail(),
+                $event->user->getName(),
+            )
+        );
     }
 }
